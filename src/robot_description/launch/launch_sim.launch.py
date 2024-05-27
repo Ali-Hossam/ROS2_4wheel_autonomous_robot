ac76c3cbce2824015cff779e5f42a1c04e9c94df
Ali-Hossam <ali.hossamx22@gmail.com>
2024-05-27 17:59:57 +0300

    added a launch file to launch both gazebo and rviz

---
+++ src/robot_description/launch/launch_sim.launch.py
@@ -0,0 +1,48 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    # get launch directory
+    bringup_dir = get_package_share_directory('robot_description')
+
+    # launch gazebo
+    gazebo_launch_path = os.path.join(bringup_dir, 'launch', 'gazebo.launch.py')
+
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gazebo_launch_path)
+    )
+    
+    # launch rviz
+    rviz_config_file_path = os.path.join(bringup_dir, 'rviz', 'sim_config.rviz')
+    rviz_config_file_arg = LaunchConfiguration('rviz_config_file',
+                                               default=rviz_config_file_path)
+    
+    rviz_launch_path = os.path.join(bringup_dir, 'launch', 'rviz.launch.py')
+    rviz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(rviz_launch_path),
+        launch_arguments={'rviz_config_file':rviz_config_file_arg}.items()
+    )
+
+    # add ros control spawner nodes
+    diff_drive_spawner = Node(
+      package='controller_manager',
+      executable='spawner',
+      arguments=['diff_cont']
+    )
+    joint_broad_spawner = Node(
+      package='controller_manager',
+      executable='spawner',
+      arguments=['joint_state_broadcaster']
+    )
+    
+    return LaunchDescription([gazebo_launch,
+                              rviz_launch,
+                              diff_drive_spawner,
+                              joint_broad_spawner])
